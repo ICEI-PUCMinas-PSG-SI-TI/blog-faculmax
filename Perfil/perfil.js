c5bce86f7ba56f7d94f70ae9ccd27411cbede50e
Davi <gdavi989@gmail.com>
2022-06-22 19:27:56 -0300

    Implementação de botão apagar conta

--- Perfil/perfil.js
+++ Perfil/perfil.js
@@ -12,6 +12,29 @@ function verLogin(){
  }
 
 verLogin();
+const deletaDados = () =>{
+    confirm('Tem certeza?')
+    if(confirm){
+        let db = JSON.parse(localStorage.getItem('db'))
+        let user = JSON.parse(localStorage.getItem('usuario'));
+        for(let i=0;i<db.registro.length;i++){
+            console.log(db.registro)
+            if(db.registro[i].nome==user.usuario){
+               
+                delete db.registro[i].nome
+                delete db.registro[i].email
+                delete db.registro[i].senha
+              
+                localStorage.removeItem('usuario');  
+                localStorage.setItem('db',JSON.stringify(db))
+                window.location.assign('../paginaPrincipal/principal.html');
+            }
+            
+        }
+       
+    }
+
+}
    
 
 function verficaDados(){
@@ -33,4 +56,6 @@ function logout(){
 }}
 
 verficaDados();
+
 document.getElementById ('deslogar').addEventListener ('click', logout);
+document.getElementById ('deleteAcc').addEventListener ('click', deletaDados);
